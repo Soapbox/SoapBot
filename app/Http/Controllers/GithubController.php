1497492d03244612bdc17740d1b6f439baf03b4a
Jaspaul Bola <jaspaul.b@gmail.com>
2016-01-30 13:18:15 -0500

    Check request signature before processing requests

--- app/Http/Controllers/GithubController.php
+++ app/Http/Controllers/GithubController.php
@@ -4,12 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class GithubController extends BaseController
 {
+    private function isSignatureValid(Request $request)
+    {
+        $secret = env('GITHUB_SECRET', '');
+        $signature = $request->header('X-Hub-Signature');
+
+        list($algorithm, $hash) = explode('=', $signature);
+
+        $payload = $request->getContent();
+        $payloadHash = hash_hmac($algorithm, $payload, $secret);
+
+        return $hash === $payloadHash;
+    }
+
     public function processWebhook(Request $request)
     {
+        if (!$this->isSignatureValid($request)) {
+            Log::error('There are hackers amoung us, unexpected signature on request. Alternatively ensure your GITHUB_SECRET environment value matches the secret provided to GitHub.');
+        }
+
         $className = Str::studly($request->header('X-Github-Event'));
 
         $webhookHandler = sprintf('App\Github\WebhookHandlers\%sHandler', $className);
